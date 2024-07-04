6fa126dfebeb5b7ec7c3f00b19c4f39726b8c03c
jtothepro <168333109+jtothepro@users.noreply.github.com>
2024-07-04 18:46:50 +0200

    Added js for two cities fixed bugs with div's

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+//Stuttgart
+let stuttgartElement = document.querySelector("#stuttgart");
+let stuttgartDateElement = stuttgartElement.querySelector(".date");
+let stuttgartTimeElement = stuttgartElement.querySelector(".time");
+let stuttgartTime = moment().tz("Europe/Berlin");
+stuttgartDateElement.innerHTML = stuttgartTime.format("MMMM Do, YYYY");
+stuttgartTimeElement.innerHTML = stuttgartTime.format(
+  "hh:mm:ss [<small>]A[</small>]"
+);
+//London
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment().tz("Europe/London");
+londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
+londonTimeElement.innerHTML = londonTime.format(
+  "hh:mm:ss [<small>]A[</small>]"
+);
