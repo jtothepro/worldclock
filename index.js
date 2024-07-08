56b187ff37a4c93dd498535aca09d4891dd1d504
jtothepro <168333109+jtothepro@users.noreply.github.com>
2024-07-08 10:55:24 +0200

    added back to home link

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function updateTimeDate() {
 
 function selectedCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -35,7 +38,8 @@ function selectedCity(event) {
           <div class="time" id="time">${cityTime.format(
             "hh:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">All cities</a>`;
 }
 
 updateTimeDate();
